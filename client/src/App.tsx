855e7eaf20a3c7c48817bbcb61574ae2b3ad765d
tinkerbell230 <tinkerbell230@tutamail.com>
2026-02-09 10:40:56 +0900

    feat:라우팅 추가

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,24 @@
-import RegisterCity from '@/components/admin/RegisterCity'
+
+import { lazy, Suspense } from 'react'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Loading from '@/components/common/Loading';
 
-
+const RegisterCity = lazy(() => import('@/pages/admin/RegisterCity'));  //동적 로딩 초기 로딩시 제외되며 화면 진입시 로딩됨
 
 
 function App() {
 
   return (
-    <>
-      <RegisterCity />
-    </>
+    <BrowserRouter>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/admin">
+            <Route path="register-city" element={<RegisterCity />} />
+          </Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
   )
 }
 
