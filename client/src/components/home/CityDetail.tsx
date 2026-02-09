d3e7f753c14ba651a776db5b13d0905b2a7a3f78
charles <mcpark4097@gmail.com>
2026-02-09 17:19:34 +0900

    도시 상세 모달 추가

--- client/src/components/home/CityDetail.tsx
+++ client/src/components/home/CityDetail.tsx
@@ -1,10 +1,91 @@
 import { City } from "@/types"
+import ArrowIcon from '@/assets/icons/arrow.svg?react';
+import FlightIcon from '@/assets/icons/airplanemode_active.svg?react';
+import VisaIcon from '@/assets/icons/airplane_ticket.svg?react';
+import VoltageIcon from '@/assets/icons/power.svg?react';
+import ClockIcon from '@/assets/icons/schedule.svg?react';
+import { Link } from 'react-router-dom';
 
 interface Prop {
     city: City;
 }
 export default function CityDetail({ city }: Prop) {
     return (
-        <div>{JSON.stringify(city)}</div>
-    )
+        <div className="w-full flex flex-col">
+            <div className="mb-18 flex">
+                <div className="flex-1 flex flex-col mr-17">
+                    <div className="mb-18">
+                        <h3 className="text-20 text-gray300 mb-10">
+                            {city.nameEn.toLocaleUpperCase()}
+                        </h3>
+                        <h2 className="text-30 font-bold">
+                            {city.country.name} {city.name}
+                        </h2>
+                    </div>
+                    <p className="text-14 leading-[160%] -tracking-[0.08px] mb-18">
+                        {city.description}
+                    </p>
+                    <div className="flex gap-x-26 mb-16">
+                        <div className="flex flex-col">
+                            <div className="text-15 tracking-[0.15px] font-semibold mb-16">
+                                <FlightIcon className="mr-8" />
+                                <span>항공</span>
+                            </div>
+                            <p className="text-gray600 tracking-[0.14px] text-14 mb-8">
+                                직항
+                            </p>
+                            <p>약 {city.flightHour} 시간</p>
+                        </div>
+                        <div className="flex flex-col">
+                            <div className="text-15 tracking-[0.15px] font-semibold mb-16">
+                                <VisaIcon className="mr-8" />
+                                <span>비자</span>
+                            </div>
+                            <p className="text-gray600 tracking-[0.14px] text-14 mb-8">
+                                {city.country.visa.required ? '필요' : '무비자'}
+                            </p>
+                            <p>{city.country.visa.duration}일</p>
+                        </div>
+                        <div className="flex flex-col">
+                            <div className="text-15 tracking-[0.15px] font-semibold mb-16">
+                                <VoltageIcon className="mr-8" />
+                                <span>전압</span>
+                            </div>
+                            <p className="text-gray600 tracking-[0.14px] text-14 mb-8">
+                                콘센트
+                            </p>
+                            <p>{city.country.voltage}V</p>
+                        </div>
+                        <div className="flex flex-col">
+                            <div className="text-15 tracking-[0.15px] font-semibold mb-16">
+                                <ClockIcon className="mr-8" />
+                                <span>시차</span>
+                            </div>
+                            <p className="text-gray600 tracking-[0.14px] text-14 mb-8">
+                                한국대비
+                            </p>
+                            <p>
+                                {city.timezoneOffset === 0
+                                    ? '없음'
+                                    : `${city.timezoneOffset}시간`}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <img
+                    src={city.thumbnail}
+                    alt={city.name}
+                    className="w-[261px] rounded-10"
+                />
+            </div>
+            <div>
+                <Link to={`/plan/${city.code}`}>
+                    <button className="w-185 bg-black text-white text-16 font-medium rounded-6 py-14 flex items-center justify-center">
+                        <span className="ml-8">일정 만들기</span>
+                        <ArrowIcon className="ml-5" />
+                    </button>
+                </Link>
+            </div>
+        </div>
+    );
 }
